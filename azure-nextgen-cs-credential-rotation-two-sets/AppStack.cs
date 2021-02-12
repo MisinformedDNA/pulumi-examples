b8c813dd00b892dba019c9f10e6cd727b055fcb7
Dan Friedman <dan@thefriedmans.org>
2021-02-12 10:48:20 -0600

    Cleanup

--- azure-nextgen-cs-credential-rotation-two-sets/AppStack.cs
+++ azure-nextgen-cs-credential-rotation-two-sets/AppStack.cs
@@ -2,29 +2,35 @@ using System;
 using System.Collections.Generic;
 using Pulumi;
 using Pulumi.AzureNextGen.Authorization.Latest;
+using Pulumi.AzureNextGen.Authorization.Latest.Inputs;
 using Pulumi.AzureNextGen.EventGrid.V20200401Preview;
 using Pulumi.AzureNextGen.EventGrid.V20200401Preview.Inputs;
+using Pulumi.AzureNextGen.Insights.V20180501Preview;
 using Pulumi.AzureNextGen.KeyVault.Latest;
 using Pulumi.AzureNextGen.KeyVault.Latest.Inputs;
 using Pulumi.AzureNextGen.Resources.Latest;
 using Pulumi.AzureNextGen.Storage.Latest;
 using Pulumi.AzureNextGen.Web.Latest;
+using Pulumi.AzureNextGen.Web.Latest.Inputs;
 using Pulumi.Random;
-using AzureNextGen = Pulumi.AzureNextGen;
-using KeyVault = Pulumi.AzureNextGen.KeyVault.Latest;
-using Storage = Pulumi.AzureNextGen.Storage.Latest;
+using ManagedServiceIdentityType = Pulumi.AzureNextGen.Web.Latest.ManagedServiceIdentityType;
+using StorageSkuArgs = Pulumi.AzureNextGen.Storage.Latest.Inputs.SkuArgs;
+using VaultSkuArgs = Pulumi.AzureNextGen.KeyVault.Latest.Inputs.SkuArgs;
+using VaultSkuName = Pulumi.AzureNextGen.KeyVault.Latest.SkuName;
 
 internal class AppStack : Stack
 {
+    private const string DefaultResourceGroupName = "credrotate21";
+
     public AppStack()
     {
         var config = new Config();
-        var resourceGroupName = config.Get("resourceGroupName") ?? "credrotate2";
-        var resourceNamePrefixParam = config.Get("resourceNamePrefix") ?? resourceGroupName;
+        var resourceGroupName = config.Get("resourceGroupName") ?? DefaultResourceGroupName;
+        var resourceNamePrefix = config.Get("resourceNamePrefix") ?? resourceGroupName;
         var clientConfig = Output.Create(GetClientConfig.InvokeAsync());
         var tenantId = clientConfig.Apply(c => c.TenantId);
 
-        var (vault, resourceGroup, storageAccount1, storageAccount2) = AddInitalResources(resourceGroupName, resourceNamePrefixParam, tenantId);
+        var (vault, resourceGroup, storageAccount1, storageAccount2) = AddInitalResources(resourceGroupName, resourceNamePrefix, tenantId);
 
         AddRotationFunction(vault, resourceGroup, storageAccount1);
     }
@@ -42,7 +48,7 @@ internal class AppStack : Stack
             Kind = "Storage",
             Location = location,
             ResourceGroupName = resourceGroup.Name,
-            Sku = new Storage.Inputs.SkuArgs { Name = "Standard_LRS" },
+            Sku = new StorageSkuArgs { Name = "Standard_LRS" },
         });
 
         var storageAccount2 = new StorageAccount("appStorageAccount2", new StorageAccountArgs
@@ -51,7 +57,7 @@ internal class AppStack : Stack
             Kind = "Storage",
             Location = location,
             ResourceGroupName = resourceGroup.Name,
-            Sku = new Storage.Inputs.SkuArgs { Name = "Standard_LRS" },
+            Sku = new StorageSkuArgs { Name = "Standard_LRS" },
         });
 
         var vault = new Vault("vault", new VaultArgs
@@ -63,12 +69,13 @@ internal class AppStack : Stack
                 EnabledForDeployment = false,
                 EnabledForDiskEncryption = false,
                 EnabledForTemplateDeployment = false,
-                Sku = new KeyVault.Inputs.SkuArgs
+                Sku = new VaultSkuArgs
                 {
                     Family = "A",
-                    Name = KeyVault.SkuName.Standard,
+                    Name = VaultSkuName.Standard,
                 },
                 TenantId = tenantId,
+                EnableSoftDelete = false,  // This should be enabled for production
             },
             ResourceGroupName = resourceGroup.Name,
             VaultName = Output.Format($"{resourceNamePrefix}-kv"),
@@ -80,41 +87,38 @@ internal class AppStack : Stack
     private static void AddRotationFunction(Vault keyVault, ResourceGroup resourceGroup, StorageAccount storageAccount)
     {
         var config = new Config();
-        var resourceGroupName = config.Get("resourceGroupName") ?? "credrotate2";
-        var functionAppNameParam = config.Get("functionAppNameParam") ?? $"{resourceGroupName}-storagekey-rotation-fnapp";
+        var resourceGroupName = config.Get("resourceGroupName") ?? DefaultResourceGroupName;
+        var functionAppName = config.Get("functionAppName") ?? $"{resourceGroupName}-storagekey-rotation-fnapp";
 
         var appServicePlanType = config.Get("appServicePlanType") ?? "Consumption Plan";
         var appServiceSku = appServicePlanType == "Consumption Plan" ? "Y1" : "P1V2";
 
-        var componentResource = new AzureNextGen.Insights.V20180501Preview.Component("componentResource", new AzureNextGen.Insights.V20180501Preview.ComponentArgs
+        var appInsights = new Component("appInsights", new ComponentArgs
         {
             Location = resourceGroup.Location,
             RequestSource = "IbizaWebAppExtensionCreate",
             ResourceGroupName = resourceGroupName,
-            ResourceName = functionAppNameParam,
+            ResourceName = functionAppName,
             Kind = "web",
             ApplicationType = "web",
-            Tags =
-            {
-            },
         });
 
-        var secretNameParam = config.Get("secretNameParam") ?? "storageKey";
-        var eventSubscriptionNameVar = $"{functionAppNameParam}-{secretNameParam}";
+        var secretName = config.Get("secretName") ?? "storageKey2";
+        var eventSubscriptionName = $"{functionAppName}-{secretName}";
 
         var prefix = new RandomString("randomPrefix", new RandomStringArgs { Special = false, Length = 13, Upper = false }).Result;
 
-        var functionStorageAccountNameVar = Output.Format($"{prefix}fnappstrg");
-        var keyVaultNameParam = config.Get("keyVaultNameParam") ?? $"{resourceGroupName}-kv";
-        var keyVaultRGParam = config.Get("keyVaultRGParam") ?? resourceGroupName;
+        var functionStorageAccountName = Output.Format($"{prefix}fnappstrg");
+        var keyVaultName = config.Get("keyVaultName") ?? $"{resourceGroupName}-kv";
+        var keyVaultRG = config.Get("keyVaultRG") ?? resourceGroupName;
 
-        var repoURLParam = config.Get("repoURLParam") ?? "https://github.com/MisinformedDNA/keyvault-rotation-storageaccountkey-powershell/" ?? "https://github.com/Azure-Samples/KeyVault-Rotation-StorageAccountKey-PowerShell.git";
-        var serverfarmResource = new AzureNextGen.Web.V20180201.AppServicePlan("serverfarmResource", new AzureNextGen.Web.V20180201.AppServicePlanArgs
+        var repoURL = config.Get("repoURL") ?? "https://github.com/Azure-Samples/KeyVault-Rotation-StorageAccountKey-PowerShell.git";
+        var serverFarm = new AppServicePlan("appServicePlan", new AppServicePlanArgs
         {
             Location = resourceGroup.Location,
             Name = $"{resourceGroupName}-rotation-fnapp-plan",
             ResourceGroupName = resourceGroupName,
-            Sku = new AzureNextGen.Web.V20180201.Inputs.SkuDescriptionArgs
+            Sku = new SkuDescriptionArgs
             {
                 Name = appServiceSku,
             },
@@ -122,11 +126,11 @@ internal class AppStack : Stack
 
         var functionAppStorage = new StorageAccount("functionApp-storage", new StorageAccountArgs
         {
-            AccountName = functionStorageAccountNameVar,
+            AccountName = functionStorageAccountName,
             Kind = "Storage",
             Location = resourceGroup.Location,
             ResourceGroupName = resourceGroupName,
-            Sku = new Storage.Inputs.SkuArgs
+            Sku = new StorageSkuArgs
             {
                 Name = "Standard_LRS",
             },
@@ -138,48 +142,48 @@ internal class AppStack : Stack
             Enabled = true,
             Kind = "functionapp",
             Location = resourceGroup.Location,
-            Name = functionAppNameParam,
+            Name = functionAppName,
             ResourceGroupName = resourceGroupName,
-            ServerFarmId = serverfarmResource.Id,
-            Identity = new AzureNextGen.Web.Latest.Inputs.ManagedServiceIdentityArgs { Type = AzureNextGen.Web.Latest.ManagedServiceIdentityType.SystemAssigned },
-            SiteConfig = new AzureNextGen.Web.Latest.Inputs.SiteConfigArgs
+            ServerFarmId = serverFarm.Id,
+            Identity = new ManagedServiceIdentityArgs { Type = ManagedServiceIdentityType.SystemAssigned },
+            SiteConfig = new SiteConfigArgs
             {
                 AppSettings =
                 {
-                    new AzureNextGen.Web.Latest.Inputs.NameValuePairArgs
+                    new NameValuePairArgs
                     {
                         Name = "AzureWebJobsStorage",
-                        Value = Output.Format($"DefaultEndpointsProtocol=https;AccountName={functionStorageAccountNameVar};AccountKey={functionAppStorageKey}"),
+                        Value = Output.Format($"DefaultEndpointsProtocol=https;AccountName={functionStorageAccountName};AccountKey={functionAppStorageKey}"),
                     },
-                    new AzureNextGen.Web.Latest.Inputs.NameValuePairArgs
+                    new NameValuePairArgs
                     {
                         Name = "FUNCTIONS_EXTENSION_VERSION",
                         Value = "~3",
                     },
-                    new AzureNextGen.Web.Latest.Inputs.NameValuePairArgs
+                    new NameValuePairArgs
                     {
                         Name = "FUNCTIONS_WORKER_RUNTIME",
                         Value = "powershell",
                     },
-                    new AzureNextGen.Web.Latest.Inputs.NameValuePairArgs
+                    new NameValuePairArgs
                     {
                         Name = "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING",
-                        Value = Output.Format($"DefaultEndpointsProtocol=https;AccountName={functionStorageAccountNameVar};EndpointSuffix=core.windows.net;AccountKey={functionAppStorageKey}"),
+                        Value = Output.Format($"DefaultEndpointsProtocol=https;AccountName={functionStorageAccountName};EndpointSuffix=core.windows.net;AccountKey={functionAppStorageKey}"),
                     },
-                    new AzureNextGen.Web.Latest.Inputs.NameValuePairArgs
+                    new NameValuePairArgs
                     {
                         Name = "WEBSITE_CONTENTSHARE",
-                        Value = functionAppNameParam.ToLower(),
+                        Value = functionAppName.ToLower(),
                     },
-                    new AzureNextGen.Web.Latest.Inputs.NameValuePairArgs
+                    new NameValuePairArgs
                     {
                         Name = "WEBSITE_NODE_DEFAULT_VERSION",
                         Value = "~10",
                     },
-                    new AzureNextGen.Web.Latest.Inputs.NameValuePairArgs
+                    new NameValuePairArgs
                     {
                         Name = "APPINSIGHTS_INSTRUMENTATIONKEY",
-                        Value = componentResource.InstrumentationKey,
+                        Value = appInsights.InstrumentationKey,
                     },
                 },
             },
@@ -189,20 +193,20 @@ internal class AppStack : Stack
             new WebAppSourceControlArgs
             {
                 Name = functionApp.Name,
-                RepoUrl = repoURLParam,
+                RepoUrl = repoURL,
                 Branch = "main",
                 IsManualIntegration = true,
                 ResourceGroupName = resourceGroupName,
             });
 
-        var eventSubscription = KvEventSubscriptionAndGrantAccess(keyVault, functionApp, functionAppSourceControl, resourceGroup, secretNameParam, "SecretExpiry");
+        var eventSubscription = KvEventSubscriptionAndGrantAccess(keyVault, functionApp, functionAppSourceControl, resourceGroup, secretName, "SecretExpiry");
 
         var expiresAt = DateTimeOffset.UtcNow.AddDays(1).ToUnixTimeSeconds();
         _ = new Secret("secret",
             new SecretArgs
             {
                 ResourceGroupName = resourceGroup.Name,
-                SecretName = "storageKey",
+                SecretName = secretName,
                 VaultName = keyVault.Name,
                 Properties = new SecretPropertiesArgs
                 {
@@ -220,8 +224,8 @@ internal class AppStack : Stack
             new RoleAssignment("functionAppAccessToStorage",
                 new RoleAssignmentArgs
                 {
-                    RoleAssignmentName = Guid.NewGuid().ToString(),
-                    Properties = new AzureNextGen.Authorization.Latest.Inputs.RoleAssignmentPropertiesArgs
+                    RoleAssignmentName = new RandomUuid("roleId", new RandomUuidArgs()).Result,
+                    Properties = new RoleAssignmentPropertiesArgs
                     {
                         PrincipalId = functionApp.Identity.Apply(f => f!.PrincipalId),
                         RoleDefinitionId = "/subscriptions/1788357e-d506-4118-9f88-092c1dcddc16/providers/Microsoft.Authorization/roleDefinitions/81a9662b-bebf-436f-a333-f67b29880f12",
